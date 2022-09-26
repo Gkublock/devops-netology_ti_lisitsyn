eb9423fb4aae7771fe71f6389de7a7c42a3efde6
TI_Lisitsyn <lisik51@gmail.com>
2022-09-26 21:30:17 +0300

    New homework

---
+++ 3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+repository = str(input("Enter repository:"))
+if repository.find("https") != -1:
+    bash_command = ["git checkout" + repository, "git status"]
+else:
+    bash_command = ["cd" + repository, "git status"]
+
+result_os = os.popen(' && '.join(bash_command)).read()
+for result in result_os.split('\n'):
+    if result.find('modified') != -1:
+        prepare_result = result.replace('modified:   ', '')
+        print(prepare_result)
